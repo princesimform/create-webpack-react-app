e195fd03255c18f7ed379b8ba1a5523f26ab5901
princesimform <prince.m@simformsolutions.com>
2023-04-20 18:07:09 +0530

    refactor code

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,3 @@
-// function App() {
-//   return (
-//     <div className='App'>
-//       <div>
-//         <a href='https://reactjs.org' target='_blank'>
-//           <img src={reactLogo} className='logo react' alt='React logo' />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className='card'>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className='read-the-docs'>
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React from "react";
 import reactLogo from "./assets/react.svg";
 import webPackLogo from "./assets/webpack.gif";
